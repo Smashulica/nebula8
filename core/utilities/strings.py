d85331454101ec4a01fcc94dc549ad4eecfa72e5
Smashulica <85994708+Smashulica@users.noreply.github.com>
2022-01-25 21:01:02 +0000

    Update strings.py

--- core/utilities/strings.py
+++ core/utilities/strings.py
@@ -4,12 +4,12 @@
 # Copyright SquirrelNetwork
 
 class Strings(object):
-    ERROR_HANDLING = "Attention there was an error in sending the command\nContact support with the command /report"
-    BAN_LOG = "<b>⚠️ #Log User Banned!</b>\n👤 User_Id: {id}\n"\
+    ERROR_HANDLING = "A aparut o eroare incercand sa rulez aceasta comanda.\nTe rog sa soliciti support folosind comanda /report"
+    BAN_LOG = "<b>⚠️ #Log Utilizator Banat!</b>\n👤 ID: {id}\n"\
               '👤 Username: <a href="tg://user?id={id}">{username}</a>\n'\
-              "👥 Group: {chat}\n"
-    SUPERBAN_LOG = '<b>🚷 #Log User Superbanned!</b>\n👤 User_Id: [{}]\n📜 Reason: {}\n🕐 Datetime: <code>{}</code>\n👮‍♀️ Operator: {} [{}]'
+              "👥 Grup: {chat}\n"
+    SUPERBAN_LOG = '<b>🚷 #Log User Global BAN!</b>\n👤 ID: [{}]\n📜 Motiv: {}\n🕐 Data si ora: <code>{}</code>\n👮‍♀️ Cine la banat: {} [{}]'
     REPORT_MSG = "⚠️ #Report\nGroup Id: <code>{}</code>\nGroup Title: {}\nMessage: <i>{}</i>\n📎 Link: {}"
-    SOURCE = "<b>Hi! my name is: {}\nMy Version is: <code>{} {}</code>\nMy repository is: {}</b>"
-    USER_INFO = '<b>⚙️ INFO USER:</b>\n👤 UserId: <code>{id}</code>\n👤 Username: <a href="tg://user?id={id}">{username}</a>\n⚠️ Warn: <code>{warn}</code>\n👥 Group: {chat}'
+    SOURCE = "<b>Salut/Buna! numele meu este: {}\nVersiunea mea este: <code>{} {}</code>\nMy group: @LupiiDinHaita {}</b>"
+    USER_INFO = '<b>⚙️ INFO USER:</b>\n👤 ID: <code>{id}</code>\n👤 Username: <a href="tg://user?id={id}">{username}</a>\n⚠️ Warnuri: <code>{warn}</code>\n👥 Grup: {chat}'
     WELCOME_BOT = "Thanks for adding me to the group\nPlease select your language => /lang\n\nRemember to make me administrator to work properly!\n\nNeed Help? => /help\n\nTo change the group settings, press the /settings command\nTo change the filters in the group, type /filters\nMy Version is: {} {}"
